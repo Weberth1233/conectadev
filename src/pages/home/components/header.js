92d9e137d0fe6cc86a63d8eada2dc2521221aacf
Weberth <weberth20-19@hotmail.com>
2021-04-19 22:48:15 -0300

    Utilizando componentes Material UI 😎.

--- src/pages/home/components/header.js
+++ src/pages/home/components/header.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
-export default class Header extends Component{
-    render(){
-        return(
-            <div>
-                <header className="header">
-                    <div className="toolbar">
-                        <div>
-                            <span>Conectar</span>
-                        </div>
-                        <div>
-                            <button>Novo Post</button>
-                            <span>img1</span>
-                            <span>img2</span>
-                        </div>
-                    </div>
-                </header>
-            </div>
-        )
+const useStyles = makeStyles({
+    appBar:{
+        boxShadow:'none',
     }
+});
+
+export default function Header() {
+    const classes = useStyles();
+    return(
+        <div>
+            <AppBar position="fixed" color="inherit" className={classes.appBar}>
+                <Toolbar>
+                <Button variant="contained" color="secondary">Novo Post</Button>;
+                    {/*<div>
+                        <a href="/">Conectar Dev - Alteração</a>
+                        <input type="text"></input>
+                    </div>
+                    <div>
+                        <Button variant="contained" color="secondary">Novo Post</Button>;
+                        <span>img1</span>
+                        <span>img2</span>
+                    </div>*/}
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 }
