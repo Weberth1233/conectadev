e71909ad8af2c7911fcc1212ab61b74a8b3bd4d6
Weberth <weberth20-19@hotmail.com>
2021-04-20 22:33:30 -0300

    Organizando o main no App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
-import Home from './pages/home'
+import Header from './pages/home/components/header'
+import NavBar from './pages/home/components/navBar'
+import Fedd from './pages/home/components/feed'
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 
+const useStyles = makeStyles({
+    root:{
+      display:'flex',
+      flexDirection:'column'
+    },
+    main:{
+      height:'100vh',
+      padding:24
+    }, 
+    toolbar:{
+      minHeight:'64px'
+    }
+})
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <Home></Home>
+      <div className={ classes.root }>
+        <Header></Header>
+        <div className={ classes.toolbar }></div>
+        <main className={ classes.main }>
+          <Container maxWidth="lg">
+            <Box display="flex">
+              <NavBar/>
+              <Fedd/>
+            </Box>
+          </Container>
+        </main>
+      </div>
     </div>
   );
 }
