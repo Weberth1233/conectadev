e00451fb0607aba6820f4fa4201f2e014d35d626
Weberth <weberth20-19@hotmail.com>
2021-04-20 22:32:52 -0300

    Criando navbar e feed e adicionando componentes 😎

---
+++ src/pages/home/components/feed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default function Feed(){
+    return(
+        <div>
+           <p>Feed</p>
+        </div>
+    )
+}
---
+++ src/pages/home/components/navBar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import ListSubheader from '@material-ui/core/ListSubheader'
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+// Custom soacing material ui
+/*Utilizando estilo proprio do theme para aplicar no paper
+padding:theme.spacing(2) o padrão do espace é 8 colocando 2 é
+multiplicado o valor por 2 8*2=16*/
+
+const useStyles = makeStyles((theme) =>({
+    root:{
+        padding:theme.spacing(2), //8*2
+        width:275,
+        marginRight:theme.spacing(2)
+    },
+    button:{
+        width:'100%'
+    }
+}));
+const tags=[
+    { id:1, name:'react.js' },
+    { id:2, name:'javascript' },
+    { id:3, name:'dotnet' },
+    { id:4, name:'php' },
+    { id:5, name:'materialdesign' },
+    { id:6, name:'webdev' }
+]
+
+export default function NavBar(){
+    const classes = useStyles();
+
+    return(
+        <Paper className={ classes.root }>
+            <Button className={classes.button} variant="outlined" color="secondary">Registrar Grátis</Button>
+            <ListSubheader>{" Tags em alta "}</ListSubheader>
+            {
+                tags.map((item)=>(
+                    <ListItem dense button key={`item-${item.id}-${item.name}`}>
+                        <ListItemText primary={`#${item.name}`}/>
+                    </ListItem>
+                ))
+            }
+            <ListItem button>
+                Exibir mais Tags     
+            </ListItem>
+        </Paper>
+    )
+}
