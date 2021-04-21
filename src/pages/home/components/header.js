e06d59ec2af675f53c32cbf5a2d52aa210498e81
Weberth <weberth20-19@hotmail.com>
2021-04-20 22:31:12 -0300

    Finalizando header

--- src/pages/home/components/header.js
+++ src/pages/home/components/header.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Button } from '@material-ui/core';
+import { Button, SvgIcon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Avatar from '@material-ui/core/Avatar';
+import { Bell } from 'react-feather';
 
 const useStyles = makeStyles({
     appBar:{
-        boxShadow:'none',
+        boxShadow:'1px',
+        backgroundColor:'#0D1117',
+    },
+    grow:{
+        flexGrow:1,
+    },
+    userSection:{
+        display:'flex',
+        alignItems:'center'
+    },
+    button:{
+        marginRight:10
+    },
+    bell:{
+        marginRight:10
     }
 });
+const styleImg={
+   maxHeight:55, 
+}
+const colorWhite={
+    color:'#fff'
+}
 
 export default function Header() {
     const classes = useStyles();
@@ -16,16 +38,15 @@ export default function Header() {
         <div>
             <AppBar position="fixed" color="inherit" className={classes.appBar}>
                 <Toolbar>
-                <Button variant="contained" color="secondary">Novo Post</Button>;
-                    {/*<div>
-                        <a href="/">Conectar Dev - Alteração</a>
-                        <input type="text"></input>
+                    <img style={styleImg} src="/images/logo192.png" alt="logo"></img>
+                    <div className={ classes.grow }></div>
+                    <div className={ classes.userSection }>
+                        <Button variant="contained" color="primary" className={ classes.button }>Novo Post</Button>;
+                        <SvgIcon className={ classes.bell }>
+                            <Bell style={ colorWhite } ></Bell>
+                        </SvgIcon>
+                        <Avatar alt="Weberth Erik" src="/"/>
                     </div>
-                    <div>
-                        <Button variant="contained" color="secondary">Novo Post</Button>;
-                        <span>img1</span>
-                        <span>img2</span>
-                    </div>*/}
                 </Toolbar>
             </AppBar>
         </div>
